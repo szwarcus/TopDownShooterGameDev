ed3bd1b321e78261399111db1482c10edc6388c4
Krzysztof Drost <diamonfrye@gmail.com>
2018-12-16 19:52:35 +0100

    The generation of sectors have been improved and the destruction of distant sectors have been added

--- Endless/Assets/Scripts/NextMap.cs
+++ Endless/Assets/Scripts/NextMap.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class NextMap : MonoBehaviour {
     /*
-     * TODO: - Poprawić sprawdzanie kidy generować nowy sektor zależnie od pozycji gracza (aktualnie nowy sektor tworzy się jeśli gracz zbiży się do krawędzi np. jak idzie w prawo to WSZYSTKIE sektory które nie mają z prawej strony sąsiada generyją jednego a nie tylko ten przy którym jest gracz)
-     *       - Dodać usuwanie sektoró w zależności od odległości między sektorem a graczem
-     *       - Uzależnić generowanie od wielkości ekranu (na razie jest tylko offset)
+     * TODO: -
      * Skrypt służy do generacji nowego sektora mapy oraz usuwania jeśli znajduje się w znacznej odległości od gracza.
      */
 
     public bool top = false, bottom = false, left = false, right = false;
-    private float width, height;
+    private float mapWidth, mapHeight;
+    private float camWidth, camHeight;
 
     [SerializeField]
-    private float offset = 16f;
+    private float offset = 1f;
 
     [SerializeField]
     private GameObject map;
@@ -31,12 +30,22 @@ public class NextMap : MonoBehaviour {
         player = GameObject.FindGameObjectWithTag("Player");
         if (player == null)
             Debug.LogError("NextMap: player not found!");
-        width = transform.GetComponent<LevelGenerator>().width;
-        height = transform.GetComponent<LevelGenerator>().height;
+        mapWidth = transform.GetComponent<LevelGenerator>().width;
+        mapHeight = transform.GetComponent<LevelGenerator>().height;
+        camHeight = 2f * Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
     }
 	// Update is called once per frame
 	void Update () {
         CheckNewMap();
+
+        if(player.transform.position.x > transform.position.x + 100 ||
+           player.transform.position.x < transform.position.x - 100 ||
+           player.transform.position.z > transform.position.z + 100 ||
+           player.transform.position.z < transform.position.z - 100)
+        {
+            DestroySector();
+        }
     }
 
     // Funkcja pozwala na sprawdzenie czy któryś z istniejących sektorów jest sąsiadem tego sektora (sąsiad rozumiany poprzez 4 kierunki góra, dół, lewo i prawo ale nie na ukos)
@@ -45,25 +54,25 @@ public class NextMap : MonoBehaviour {
         for(int i = 0; i < map.transform.parent.transform.childCount; i++) // wykonaj dla każdego sektora
         {
             Vector3 childPos = map.transform.parent.transform.GetChild(i).transform.position;
-            if(childPos.z == transform.position.z + height && childPos.x == transform.position.x)
+            if(childPos.z == transform.position.z + mapHeight && childPos.x == transform.position.x)
             {
 //                Debug.Log("Sąsiad u góry");
                 top = true;
                 map.transform.parent.transform.GetChild(i).GetComponent<NextMap>().bottom = true;
             }
-            else if (childPos.z == transform.position.z - height && childPos.x == transform.position.x)
+            else if (childPos.z == transform.position.z - mapHeight && childPos.x == transform.position.x)
             {
 //                Debug.Log("Sąsiad na dole");
                 bottom = true;
                 map.transform.parent.transform.GetChild(i).GetComponent<NextMap>().top = true;
             }
-            else if (childPos.x == transform.position.x + width && childPos.z == transform.position.z)
+            else if (childPos.x == transform.position.x + mapWidth && childPos.z == transform.position.z)
             {
 //                Debug.Log("Sąsiad na prawo");
                 right = true;
                 map.transform.parent.transform.GetChild(i).GetComponent<NextMap>().left = true;
             }
-            else if (childPos.x == transform.position.x - width && childPos.z == transform.position.z)
+            else if (childPos.x == transform.position.x - mapWidth && childPos.z == transform.position.z)
             {
 //                Debug.Log("Sąsiad na lewo");
                 left = true;
@@ -75,16 +84,13 @@ public class NextMap : MonoBehaviour {
     // Funkcja sprawdza czy nowy sektor powinien zostać stworzony i tworzy go
     private void CheckNewMap()
     {
-        float topDistance = transform.position.z - player.transform.position.z + height / 2;
-        float bottomDistance = -transform.position.z + player.transform.position.z + height / 2;
-        
-        
-        float rightDistance = transform.position.x - player.transform.position.x + width / 2;
-        float leftDistance = -transform.position.x + player.transform.position.x + width / 2;
-        // nalezy zmianić formuły w if-ach poniżej
-        if (topDistance < offset && top == false && - transform.position.x - width < player.transform.position.x)
+        if (player.transform.position.z > transform.position.z + mapHeight / 2 - offset - camHeight / 2 &&
+            player.transform.position.z < transform.position.z + mapHeight &&
+            player.transform.position.x > transform.position.x - mapWidth / 2 + offset - camWidth / 2 &&
+            player.transform.position.x < transform.position.x + mapWidth / 2 - offset + camWidth / 2 &&
+            top == false)
         {
-            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + height);
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + mapHeight);
             GameObject clone = Instantiate(map, newPos, Quaternion.identity, transform.parent);
             while(clone.transform.childCount != 0)
             {
@@ -94,9 +100,13 @@ public class NextMap : MonoBehaviour {
             clone.name = "sektor";
             clone.GetComponent<NextMap>().CheckNeighbors();
         }
-        if (bottomDistance < offset && bottom == false)
+        if (player.transform.position.z < transform.position.z - mapHeight / 2 + offset + camHeight / 2 &&
+            player.transform.position.z > transform.position.z - mapHeight &&
+            player.transform.position.x > transform.position.x - mapWidth / 2 + offset - camWidth / 2 &&
+            player.transform.position.x < transform.position.x + mapWidth / 2 - offset + camWidth / 2 &&
+            bottom == false)
         {
-            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - height);
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - mapHeight);
             GameObject clone = Instantiate(map, newPos, Quaternion.identity, transform.parent);
             while (clone.transform.childCount != 0)
             {
@@ -106,9 +116,13 @@ public class NextMap : MonoBehaviour {
             clone.name = "sektor";
             clone.GetComponent<NextMap>().CheckNeighbors();
         }
-        if (leftDistance < offset && left == false)
+        if (player.transform.position.x < transform.position.x - mapWidth / 2 + offset + camWidth / 2 &&
+            player.transform.position.x > transform.position.x - mapWidth &&
+            player.transform.position.z < transform.position.z + mapHeight / 2 - offset + camHeight / 2 &&
+            player.transform.position.z > transform.position.z - mapHeight / 2 + offset - camHeight / 2 &&
+            left == false)
         {
-            Vector3 newPos = new Vector3(transform.position.x - width, transform.position.y, transform.position.z);
+            Vector3 newPos = new Vector3(transform.position.x - mapWidth, transform.position.y, transform.position.z);
             GameObject clone = Instantiate(map, newPos, Quaternion.identity, transform.parent);
             while (clone.transform.childCount != 0)
             {
@@ -118,9 +132,13 @@ public class NextMap : MonoBehaviour {
             clone.name = "sektor";
             clone.GetComponent<NextMap>().CheckNeighbors();
         }
-        if (rightDistance < offset && right == false)
+        if (player.transform.position.x > transform.position.x + mapWidth / 2 - offset - camWidth / 2 &&
+            player.transform.position.x < transform.position.x + mapWidth &&
+            player.transform.position.z < transform.position.z + mapHeight / 2 - offset + camHeight / 2 &&
+            player.transform.position.z > transform.position.z - mapHeight / 2 + offset - camHeight / 2 &&
+            right == false)
         {
-            Vector3 newPos = new Vector3(transform.position.x + width, transform.position.y, transform.position.z);
+            Vector3 newPos = new Vector3(transform.position.x + mapWidth, transform.position.y, transform.position.z);
             GameObject clone = Instantiate(map, newPos, Quaternion.identity, transform.parent);
             while (clone.transform.childCount != 0)
             {
@@ -130,5 +148,33 @@ public class NextMap : MonoBehaviour {
             clone.name = "sektor";
             clone.GetComponent<NextMap>().CheckNeighbors();
         }
+    }
+
+    // funkcja usuwa dany sektor jeśli znajduję się za daleko od gracza w celu optymalizacji 
+    private void DestroySector()
+    {
+        // potrzebne najpierw jest zmiana wartości boolowskich o tym czy dany sektor ma sąsiada, we wszystkich sąsiadach tego sektoru(przeciwięństwo CheckNeighbors())
+        for (int i = 0; i < transform.parent.transform.childCount; i++) // wykonaj dla każdego sektora
+        {
+            Debug.Log(i + " " + transform.parent.transform.childCount);
+            Vector3 childPos = transform.parent.transform.GetChild(i).transform.position;
+            if (childPos.z == transform.position.z + mapHeight && childPos.x == transform.position.x)
+            {
+                transform.parent.transform.GetChild(i).GetComponent<NextMap>().bottom = false;
+            }
+            else if (childPos.z == transform.position.z - mapHeight && childPos.x == transform.position.x)
+            {
+                transform.parent.transform.GetChild(i).GetComponent<NextMap>().top = false;
+            }
+            else if (childPos.x == transform.position.x + mapWidth && childPos.z == transform.position.z)
+            {
+                transform.parent.transform.GetChild(i).GetComponent<NextMap>().left = false;
+            }
+            else if (childPos.x == transform.position.x - mapWidth && childPos.z == transform.position.z)
+            {
+                transform.parent.transform.GetChild(i).GetComponent<NextMap>().right = false;
+            }
+        }
+        Destroy(gameObject);
     }
 }
